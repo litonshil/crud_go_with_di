46ff56b88d72a6b0c9b9def6d6ead55b02463f45
litonshil <72643074+litonshil@users.noreply.github.com>
2022-07-26 15:37:16 +0600

    minor changes

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -5,10 +5,10 @@ import (
 )
 
 type User struct {
-	Id       	int			`gorm:"primary_key;AUTO_INCREMENT"`
-	Email		string		`json:"email" validate:"required,email"`
-	Name     	string      `json:"name" validate:"required,min=2,max=15"`
-	Address		string		`json:"address"`
-	Type		string		`json:"type" validate:"required"`	
-	Password  string		`json:"password" validate:"required"`
+	Id       int    `gorm:"primary_key;AUTO_INCREMENT"`
+	Email    string `json:"email" validate:"required,email"`
+	Name     string `json:"name" validate:"required,min=2,max=15"`
+	Address  string `json:"address"`
+	Type     string `json:"type" validate:"required"`
+	Password string `json:"password" validate:"required"`
 }
--- pkg/types/user.go
+++ pkg/types/user.go
@@ -1,11 +1,10 @@
 package types
 
 type User struct {
-	Email     string `json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required"`
-	Type      string `json:"type" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	Type     string `json:"type" validate:"required"`
 }
-
 
 type Token struct {
 	User_Token        string
