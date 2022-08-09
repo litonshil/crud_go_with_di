083e85ced70a83dcb570ca635a5271ccbd442d18
litonshil <72643074+litonshil@users.noreply.github.com>
2022-08-09 13:01:19 +0600

    docker compose file updated

--- config/config.go
+++ config/config.go
@@ -16,7 +16,6 @@ type Config struct {
 	SmtpPort     string `mapstructure:"SMTP_PORT"`
 	SmtpPassword string `mapstructure:"SMTP_PASSWORD"`
 	SecretKey    string `mapstructure:"SECRETKEY"`
-	MysqlUser    string `mapstructure:"MYSQL_USER"`
 }
 
 func init_config() (config Config, err error) {
